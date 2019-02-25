38fc8abed87f6b1eadd8fa7bd842881e1aab63f1
Grash <grashuriza@gmail.com>
2019-02-25 10:07:25 -0500

    Lab5

---
+++ Labs/Lab05/HandlesText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor; 
+
+public class HandlesText : MonoBehaviour
+{
+    public string text = "text"; 
+    public Vector3 TextOffset = Vector3.zero; 
+    
+    void OnDrawGizmos()
+    {
+        Handles.Label(transform.position + TextOffset, text);
+    }
+}
